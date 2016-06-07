2f53e8d4f38fc6ae1b44eb3a8d47be1357eb81e1
Maz <mazicky@gmail.com>
2016-06-07 23:07:46 +0900

    Pass ASAP

--- src/utils/anagrams.js
+++ src/utils/anagrams.js
@@ -1,13 +1,29 @@
 export function anagrams(input) {
   const ret = []
-  ret.push(input)
-  if (input.length == 2) {
+
+  if (input.length === 3) {
+    for (let i=0; i<3; ++i) {
+      const header = input.charAt(i)
+      const remain = anagrams(subStringExcept(input, header))
+      const candidates = remain.map(sub => {
+        return header+sub
+      })
+      ret.push(...candidates)
+    }
+  } else {
+    ret.push(input)
+  }
+
+  if (input.length === 2) {
     let reverse = ''
     for (let i=input.length - 1; i>= 0; --i) {
       reverse += input.charAt(i)
     }
     ret.push(reverse)
   }
+
+  return ret
+}
 
 export function subStringExcept(string, except) {
   let ret = ''
