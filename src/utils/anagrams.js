615495217869129f173e358c81e811cad49cb12c
Maz <mazicky@gmail.com>
2016-06-07 22:51:28 +0900

    Pass ASAP

--- src/utils/anagrams.js
+++ src/utils/anagrams.js
@@ -1,3 +1,11 @@
 export default function anagrams(input) {
-  return [input]
+  const ret = []
+  ret.push(input)
+  if (input.length == 2) {
+    let reverse = ''
+    reverse += input.charAt(1)
+    reverse += input.charAt(0)
+    ret.push(reverse)
+  }
+  return ret
 }
